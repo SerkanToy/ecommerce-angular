adaef5a173194fd0f630be04833ad95a2eaf567a
Serkan TOY <stoy@windowslive.com>
2026-07-19 15:40:28 +0300

    LoginDto Oluşturuldu ve Yapılandırıldı

---
+++ ecommerce.api/Models/DTOs/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ecommerce.api.Models.DTOs
+{
+    public class LoginDto
+    {
+
+
+        [Required(ErrorMessage = "Boş Bırakmayın.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Boş Bırakmayın.")]
+        public string Password { get; set; }
+    }
+}
