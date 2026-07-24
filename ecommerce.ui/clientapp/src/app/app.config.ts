72fadb1484c4a19fd8273740320d2e109955a01d
Serkan TOY <stoy@windowslive.com>
2026-07-24 19:45:37 +0300

    Angular dependencies and imports

--- ecommerce.ui/clientapp/src/app/app.config.ts
+++ ecommerce.ui/clientapp/src/app/app.config.ts
@@ -2,10 +2,20 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideToastr({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true
+    }),
+    provideHttpClient(withInterceptors([]))
   ]
 };
